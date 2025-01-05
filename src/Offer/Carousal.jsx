551399b93108cca4dd531da11d50303ebee7100c
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-06 09:25:21 +1100

    prashrijan commit

--- src/Offer/Carousal.jsx
+++ src/Offer/Carousal.jsx
@@ -1,20 +1,23 @@
 "use client";
 
 import { Carousel } from "flowbite-react";
+import test from "../assets/test.png";
 
 import React from "react";
 
 const Carousal = () => {
   return (
-    <div className="w-[80%] h-[auto] min-h-[300px]">
-      <Carousel
-        slideInterval={3000}
-        className="carousal h-[auto] min-h-[300px]"
-      >
+    <div className="w-[80%] h-auto min-h-[300px]">
+      <Carousel slideInterval={70000} className="carousal h-auto min-h-[300px]">
+        <div>
+          <a href="">
+            <img src={test} alt="dsfgsd" className="w-full object-cover" />
+          </a>
+        </div>
         <div>
           <a href="">
             <img
-              src="/assets/images/OfferTemplate1.png"
+              src="public/assets/images/OfferTemplate1.png"
               alt="dsfgsd"
               className="w-full h-auto object-cover"
             />
@@ -23,16 +26,7 @@ const Carousal = () => {
         <div>
           <a href="">
             <img
-              src="/assets/images/OfferTemplate1.png"
-              alt="dsfgsd"
-              className="w-full h-auto object-cover"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img
-              src="/assets/images/OfferTemplate1.png"
+              src="public/assets/images/OfferTemplate1.png"
               alt="dsfgsd"
               className="w-full h-auto object-cover"
             />
