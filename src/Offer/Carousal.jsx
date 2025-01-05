b1ac84bd327ce8997ac0a9d7e7bb6d5daeaef116
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-06 09:43:39 +1100

    carousel fix

--- src/Offer/Carousal.jsx
+++ src/Offer/Carousal.jsx
@@ -1,37 +1,22 @@
 "use client";
 
 import { Carousel } from "flowbite-react";
-import test from "../assets/test.png";
 
 import React from "react";
 
 const Carousal = () => {
   return (
-    <div className="w-[80%] h-auto min-h-[300px]">
-      <Carousel slideInterval={70000} className="carousal h-auto min-h-[300px]">
-        <div>
-          <a href="">
-            <img src={test} alt="dsfgsd" className="w-full object-cover" />
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img
-              src="public/assets/images/OfferTemplate1.png"
-              alt="dsfgsd"
-              className="w-full h-auto object-cover"
-            />
-          </a>
-        </div>
-        <div>
-          <a href="">
-            <img
-              src="public/assets/images/OfferTemplate1.png"
-              alt="dsfgsd"
-              className="w-full h-auto object-cover"
-            />
-          </a>
-        </div>
+    <div className="w-full h-full">
+      <Carousel>
+        <a href="">
+          <img src="./assets/images/OfferTemplate1.png" alt="..." />
+        </a>
+        <a href="">
+          <img src="./assets/images/OfferTemplate1.png" alt="..." />
+        </a>
+        <a href="">
+          <img src="./assets/images/OfferTemplate1.png" alt="..." />
+        </a>
       </Carousel>
     </div>
   );
