221f01de2d14a14c8d51c456721b63772e169e30
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-06 20:31:44 +1100

    getting undefined error in the flash sale card need to fix

--- src/Offer/FlashSale/FlashSaleCard.jsx
+++ src/Offer/FlashSale/FlashSaleCard.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 
 const FlashSaleCard = ({ item }) => {
-  console.log("Item in FlashSaleCard:", item);
+  if (!item) {
+    console.error("Received undefined item in FlashSaleCard");
+    return <div>Error: Item data is missing</div>;
+  }
+
   return (
     <a href="" className="inline-block hover:border-2 p-2">
       <div className="flex flex-col justify-start">
         <img
           src="/assets/images/watchProduct.png"
-          alt=""
+          alt={item.name || "Product"}
           className="h-[150px] w-[150px]"
         />
-        <p>{}</p>
-        <span className="text-[#f57224]">Rs. 34</span>
+        <p>{item.name}</p>
+        <span className="text-[#f57224]">Rs. {item.newPrice}</span>
         <span className="flex gap-2">
-          <p className="line-through text-[#9e9e9e  ]">Rs. 50</p>
-          <p>-25%</p>
+          <p className="line-through text-[#9e9e9e]">Rs. {item.oldPrice}</p>
+          <p>-{item.discountPercentage}%</p>
         </span>
       </div>
     </a>
