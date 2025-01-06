dcd4caafb31df493d4d8cc56235f57b46b08144c
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-06 23:46:11 +1100

    categories cards

---
+++ src/CategoriesItems.js
@@ -0,0 +1,20 @@
+const categories = [
+  "Electronics",
+  "Fashion",
+  "Home & Kitchen",
+  "Health & Beauty",
+  "Sports & Outdoors",
+  "Toys & Games",
+  "Books",
+  "Automotive",
+  "Groceries",
+  "Pet Supplies",
+  "Baby Products",
+  "Jewelry & Watches",
+  "Office Supplies",
+  "Music & Movies",
+  "Tools & Hardware",
+  "Gardening",
+];
+
+export default categories;
