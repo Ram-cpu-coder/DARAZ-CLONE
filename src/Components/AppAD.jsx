9461080b88d01db9ce108d2841d6eb6507af9fbf
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-08 16:17:24 +1100

    ad components chang

--- src/Components/AppAD.jsx
+++ src/Components/AppAD.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FaStar } from "react-icons/fa6";
 
 const AppAD = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-full w-[20%] border">
+    <div className="flex flex-col justify-center items-center h-full w-[20%] border bg-custom-gradient gap-4 p-2">
       {/* logo and title  */}
 
       <div className="flex justify-center items-center gap-2">
@@ -15,24 +16,40 @@ const AppAD = () => {
       </div>
 
       {/* rating and download titles */}
-      <div className="appAdbg">
-        <p>4.8 Rated</p>
-        <p>Download App</p>
-        <div className="flex flex-col">
-          <div>
-            <img src="" alt="" />
+      <div className="appAdbg bg-adBg w-full bg-no-repeat bg-cover overflow-hidden h-[158px] flex flex-col text-white px-2 py-1 gap-1">
+        <div className="ratings flex items-center gap-1 text-sm">
+          <FaStar />
+          <p>4.8 Rated</p>
+        </div>
+
+        <p className="text-center text-sm font-bold">Download App</p>
+        <div className="flex flex-col p-4 gap-2">
+          <div className="flex items-center gap-2">
+            <img
+              src="https://img.lazcdn.com/g/tps/imgextra/i2/O1CN01n3PMa828kJZVuCbPp_!!6000000007970-2-tps-72-72.png_150x150q80.png"
+              className="size-8"
+            />
             <p>Free Delivery</p>
           </div>
-          <div>
-            <img src="" alt="" />
-            <p>Free Delivery</p>
+          <div className="flex items-center gap-2">
+            <img
+              src="https://img.lazcdn.com/g/tps/imgextra/i3/O1CN01J03SMW1lebTE7xkaN_!!6000000004844-2-tps-72-72.png_150x150q80.png"
+              className="size-8"
+            />
+            <p>Limited Time</p>
           </div>
         </div>
       </div>
       {/* qr code and the link to download */}
       <div>
         {/* qr code */}
-        <div></div>
+        <div>
+          <img
+            src="https://img.drz.lazcdn.com/g/tps/imgextra/i2/O1CN01da6D0K1Khogk708ZP_!!6000000001196-0-tps-150-150.jpg_360x360q80.jpg"
+            alt=""
+            className="size-20"
+          />
+        </div>
         {/* links to download */}
         <div>
           {/* app store link */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        adBg: 'url("https://img.lazcdn.com/g/tps/imgextra/i2/O1CN01tceZus1IewufOY1tZ_!!6000000000919-2-tps-364-316.png")',
+        "custom-gradient": `
+          linear-gradient(90deg, #fff, hsla(0, 0%, 100%, 0)),
+          linear-gradient(179.27deg, #ffedd6 0.83%, #ffc3e3 46.78%, #fff 99.57%),
+          linear-gradient(0deg, #fff, #fff)
+        `,
+      },
+    },
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
-}
+  plugins: [flowbite.plugin()],
+};
