499d7596629f4f1db0e64c6058ceb9e6cb5131b9
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-01-25 14:24:23 +1100

    country.js and iterating the country in become a seller page

---
+++ src/Data/Country.js
@@ -0,0 +1,23 @@
+const country = [
+    {
+        country: "Nepal",
+        imgSrc: "https://img.lazcdn.com/us/domino/0a1acdf4-47e1-41d6-9247-6d67b0025826_NP-86-86.png"
+    },
+    {
+        country: "Pakistan",
+        imgSrc: "https://img.lazcdn.com/us/domino/e18dc75b-c2f1-4f27-8df2-ea98af38369c_NP-84-84.png"
+    },
+    {
+        country: "Bangladesh",
+        imgSrc: "https://img.lazcdn.com/us/domino/99549a48-8903-40cf-946e-c717069f2826_NP-84-84.png"
+    },
+    {
+        country: "Sri Lanka",
+        imgSrc: "https://img.lazcdn.com/us/domino/502c6171-a00f-4f86-9155-bb0d673bf613_NP-84-84.png"
+    },
+    {
+        country: "Myanmar",
+        imgSrc: "https://img.lazcdn.com/us/domino/175d06e3-1efe-436b-bc3e-9631ae37768d_NP-84-84.png"
+    },
+]
+export default country;
--- src/Pages/BecomeASeller.jsx
+++ src/Pages/BecomeASeller.jsx
@@ -1,40 +1,44 @@
-import React from "react"
+import React from "react";
+import { Dropdown } from "flowbite-react";
+import country from "../Data/country";
 const BecomeASeller = () => {
+  // const eachItem = country.map((item) => {
+  //   return item.country && item.imgSrc;
+  // });
+  // console.log(eachItem);
   return (
-    <nav className='h-[75px] w-screen flex items-center justify-between bg-white fixed top-0 z-50'>
+    <nav className="h-[75px] w-screen flex items-center justify-between bg-white fixed top-0 z-50">
       <div>
         <img
-          src='https://img.alicdn.com/imgextra/i3/O1CN01ovcTAV1WBY7oOmozh_!!6000000002750-55-tps-323-127.svg'
-          alt=''
-          className='h-[60px] w-auto mx-4 m-2'
+          src="https://img.alicdn.com/imgextra/i3/O1CN01ovcTAV1WBY7oOmozh_!!6000000002750-55-tps-323-127.svg"
+          alt=""
+          className="h-[60px] w-auto mx-4 m-2"
         />
       </div>
-
-      <div>
-        <select className='border-none m-2 min-w-[100px] focus:ring-white'>
-          <option>
-            <img src='' alt=''></img>Nepal
-          </option>
-          <option>
-            <img src='' alt=''></img>Pakistan
-          </option>
-          <option>
-            <img src='' alt=''></img>Bangladesh
-          </option>
-          <option>
-            <img src='' alt=''></img>Sri Lanka
-          </option>
-          <option>
-            <img src='' alt=''></img>Myanmar
-          </option>
-        </select>
-        <select className=' m-2 mr-5 border-none focus:ring-white'>
+      <Dropdown
+        label="Dropdown button"
+        dismissOnClick={false}
+        className="border-none m-2 min-w-[80px] focus:ring-white"
+      >
+        {country.map((item) => {
+          return (
+            <Dropdown.Item className="flex gap-3 justify-center">
+              <img src={item.imgSrc} alt={item.country} className="h-[25px]" />
+              {item.country}
+            </Dropdown.Item>
+          );
+        })}
+      </Dropdown>
+      {/* <div>
+        
+         
+        <select className=" m-2 mr-5 border-none focus:ring-white">
           <option>English</option>
           <option>Nepali</option>
         </select>
-      </div>
+      </div> */}
     </nav>
-  )
-}
+  );
+};
 
-export default BecomeASeller
+export default BecomeASeller;
